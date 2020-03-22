274586f9741bdf6599212a7bb197c3d510b11737
Alex Gohorel <3317203+agohorel@users.noreply.github.com>
2020-03-22 15:17:56 -0400

    initial commit - basic single file nearest neighbor upscaler

---
+++ neighbor_upscale.sh
@@ -0,0 +1,11 @@
+echo enter the path to the file you would like to upscale
+read FILE_PATH
+
+echo enter the factor you would like to scale the img
+read SCALE_FACTOR
+
+echo enter the filename you would like to save the output as.
+read OUTPUT_PATH
+
+ffmpeg -i $FILE_PATH -vf scale=iw*$SCALE_FACTOR:-1:flags=neighbor $OUTPUT_PATH
+                               
